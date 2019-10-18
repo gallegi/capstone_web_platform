6e5b6e5bbeabda785d377839003b4510ea32fb12
Long Vu <vulong22121999@gmail.com>
2019-10-18 14:20:33 +0700

    update masterpage

---
+++ vnpost_ocr_system/Controllers/InvitationCard/DisplayStatusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vnpost_ocr_system.Controllers.InvitationCard
+{
+    public class DisplayStatusController : Controller
+    {
+        // GET: DisplayStatus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ vnpost_ocr_system/Controllers/InvitationCard/InputInformationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vnpost_ocr_system.Controllers.InvitationCard
+{
+    public class InputInformationController : Controller
+    {
+        // GET: InputInformation
+        public ActionResult Index()
+        {
+            return View("/Views/InvitationCard/InputInformation.cshtml");
+        }
+    }
+}
