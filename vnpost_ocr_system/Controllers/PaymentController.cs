d39aa134a4d784686be500e0cd04d57f06053c0d
bachbop9999 <39481286+bachbop9999@users.noreply.github.com>
2019-10-19 15:36:37 +0700

    add payment controller

---
+++ vnpost_ocr_system/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vnpost_ocr_system.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        
+        public ActionResult Index()
+        {
+            return View("/Views/Payment/Payment.cshtml");
+        }
+    }
+}
