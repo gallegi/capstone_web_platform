5c8e7598af98f5fe0a404c5177502bd6b9afe33c
bachbop9999 <39481286+bachbop9999@users.noreply.github.com>
2019-11-05 10:26:22 +0700

    update S6 (add 1 section.) and S12 (choose city, distric,t..)

---
+++ vnpost_ocr_system/CustomJS/Document/CustomDisplayStatisticChart.js
@@ -0,0 +1,17 @@
+﻿function city(value) {
+
+    document.getElementById('city').innerHTML = value;
+}
+function district(value) {
+
+    document.getElementById('district').innerHTML = value;
+}
+function org(value) {
+
+    document.getElementById('org').innerHTML = value;
+}
+function procedure(value) {
+
+    document.getElementById('procedure').innerHTML = value;
+}
+
