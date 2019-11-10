11956c64298a9b19fe4cc88cf51fc7b5aee10d4a
bachbop9999 <39481286+bachbop9999@users.noreply.github.com>
2019-11-10 16:28:17 +0700

    update s12, s2.1

--- vnpost_ocr_system/CustomJS/Document/DisplayStatisticChart.js
+++ vnpost_ocr_system/CustomJS/Document/DisplayStatisticChart.js
@@ -1,132 +1,74 @@
-﻿
-
-$(function () {
+﻿$(function () {
     "use strict";
     // ------------------------------
-    // Basic line chart
+    // Basic bar chart
     // ------------------------------
     // based on prepared DOM, initialize echarts instance
-    var myChart = echarts.init(document.getElementById('basic-line'));
+    var myChart = echarts.init(document.getElementById('basic-bar'));
 
     // specify chart configuration item and data
     var option = {
-
         // Setup grid
         grid: {
             left: '1%',
-            right: '6%',
-            bottom: '4%',
+            right: '15%',
+            bottom: '3%',
             containLabel: true
         },
+
+        // Add Tooltip
         tooltip: {
             trigger: 'axis'
         },
 
-
-        // Add custom colors
+       
+       
         color: ['#d675d0', '#58bbff', '#fcb051'],
-
-        // Enable drag recalculate
         calculable: true,
-
-        // Horizontal Axiz
         xAxis: [
             {
-                name: 'Ngày',
+                name: 'Khoảng thời gian',
                 type: 'category',
-                boundaryGap: false,
-                data: ['30', '29', '29', '27', '26', '25', '24', '23', '22', '21', '20',
-                    '19', '18', '17', '16', '15', '14', '13', '12', '11', '10', '9',
-                    '8', '7', '6', '5', '4', '3', '2', '1']
+                data: ['03/10/2018 - 10/10/2018','05/11/2018 - 12/11/2018']
             }
         ],
-
-        // Vertical Axis
         yAxis: [
             {
                 name: 'Số lượng',
                 type: 'value',
-                boundaryGap: false,
-                data: ['0', '50', '150', '200', '250', '300', '350', '400', '450', '500', '550', '600']
+                data: []
             }
         ],
-
-        // Add Series
         series: [
             {
-                name: 'Đang tiếp nhận',
-                type: 'line',
-                data: [Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-
-                ],
-
-
-                lineStyle: {
-                    normal: {
-                        width: 3,
-                        shadowColor: 'rgba(0,0,0,0.1)',
-                        shadowBlur: 10,
-                        shadowOffsetY: 10
-                    }
-                },
+                name: 'Hồ sơ đang chờ tiếp nhận',
+                type: 'bar',
+                data: [30,45],
+                
+                
             },
             {
-
-                name: 'Đã tiếp nhận',
-                type: 'line',
-                data: [Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-
-                ],
-
-
-                lineStyle: {
-                    normal: {
-                        width: 3,
-                        shadowColor: 'rgba(0,0,0,0.1)',
-                        shadowBlur: 10,
-                        shadowOffsetY: 10
-                    }
-                },
+                name: 'Hồ sơ đã tiếp nhập',
+                type: 'bar',
+                data: [12,56],
+               
+               
             },
             {
-
-                name: 'Đã xử lý',
-                type: 'line',
-                data: [Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-                       Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500, Math.random() * 500,
-
-                ],
+                name: 'Hồ sơ đã xử lý',
+                type: 'bar',
+                data: [24, 33],
 
 
-                lineStyle: {
-                    normal: {
-                        width: 3,
-                        shadowColor: 'rgba(0,0,0,0.1)',
-                        shadowBlur: 10,
-                        shadowOffsetY: 10
-                    }
-                },
             }
+            
         ]
     };
     // use configuration item and data specified to show chart
     myChart.setOption(option);
 
 
+   
 
 
     //------------------------------------------------------
@@ -144,89 +86,9 @@ $(function () {
 
                 // Resize chart
                 myChart.resize();
-
-            }, 200);
-        }
-    });
-});
-
-//========================================================================================================================
-$(function () {
-    "use strict";
-    // ------------------------------
-    // Basic pie chart
-    // ------------------------------
-    // based on prepared DOM, initialize echarts instance
-    var basicpieChart = echarts.init(document.getElementById('basic-pie'));
-    var dataStyle = {
-        normal: {
-            label: { show: false },
-            labelLine: { show: false }
-        }
-    };
-    var option = {
-        // Add title
-
-
-        // Add tooltip
-        tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b}: {c} ({d}%)"
-        },
-
-        // Add legend
-        //legend: {
-        //    orient: 'vertical',
-        //    x: 'left',
-        //    data: ['IE', 'Opera', 'Safari', 'Firefox', 'Chrome']
-        //},
-
-        // Add custom colors
-        color: ['#d675d0', '#58bbff', '#fcb051'],
-
-        // Display toolbox
-
-
-        // Enable drag recalculate
-        calculable: true,
-
-        // Add series
-        series: [{
-            name: 'Thống kê',
-            type: 'pie',
-            radius: '85%',
-            center: ['50%', '57.5%'],
-            clockWise: false,
-            itemStyle: dataStyle,
-            data: [
-                { value: 240, name: 'Đang tiếp nhận' },
-                { value: 320, name: 'Đã tiếp nhận' },
-                { value: 200, name: 'Đã xử lý' },
-
-            ]
-        }]
-    };
-
-    basicpieChart.setOption(option);
-
-
-
-    //------------------------------------------------------
-    // Resize chart on menu width change and window resize
-    //------------------------------------------------------
-    $(function () {
-
-        // Resize chart on menu width change and window resize
-        $(window).on('resize', resize);
-        $(".sidebartoggler").on('click', resize);
-
-        // Resize function
-        function resize() {
-            setTimeout(function () {
-
-                // Resize chart
-                basicpieChart.resize();
-                poleChart.resize();
+                stackedChart.resize();
+                stackedbarcolumnChart.resize();
+                barbasicChart.resize();
             }, 200);
         }
     });
--- vnpost_ocr_system/CustomJS/InputInforScreen21.js
+++ vnpost_ocr_system/CustomJS/InputInforScreen21.js
@@ -23,13 +23,13 @@
     // Private function for creating the input element
     function create_btn(that, settings) {
         // The input icon element
-        var picker_btn_icon = $('<i class="test2"></i>');
+        var picker_btn_icon = $('<i class=""></i>');
 
         // The actual file input which stays hidden
         var picker_btn_input = $('<input  type="file" name="' + settings.name + '" />');
 
         // The actual element displayed
-        var picker_btn = $('<div class="' + settings.class + ' img-upload-btn"></div>')
+        var picker_btn = $('<div class="' + settings.class + '"></div>')
             .append(picker_btn_icon)
             .append(picker_btn_input);
 
@@ -60,7 +60,7 @@
         var picker_preview_image = $('<img src="' + src + '" class="test" />');
 
         // The remove image button
-        var picker_preview_remove = $('<button class="btn btn-link"><small>Remove</small></button>');
+        var picker_preview_remove = $('<button class="btn bt-color-common  btn-link"><small>Remove</small></button>');
 
         // The preview element
         var picker_preview = $('<div class="text-center"></div>')
@@ -81,3 +81,17 @@
 $(document).ready(function () {
     $('.img-picker').imagePicker({ name: 'images' });
 })
+
+function checkFilter() {
+    var item1 = document.getElementById("item1").value;
+    var item2 = document.getElementById("item2").value;
+    var item3 = document.getElementById("item3").value;
+    var item4 = document.getElementById("item4").value;
+    
+   
+    
+    if (item1 !== "Tỉnh/Thành phố" && item2 !== "Quận/Huyện" && item3 !== "Tên CQHC" && item4 !== "Tên thủ tục") {
+        //alert(item1 + "-" + item2 + "-" + item3 + "-" + item4);
+        document.getElementById("contact").style.display = "block";
+    }
+}
