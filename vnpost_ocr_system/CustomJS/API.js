5bd18141dc238b6497616498b9075328a5fdd71e
chidorin01 <48520915+chidorin01@users.noreply.github.com>
2019-11-11 23:31:07 +0700

    Fix lại bảng có thể edit trong screen s13.1 và s13.2

---
+++ vnpost_ocr_system/CustomJS/API.js
@@ -0,0 +1,33 @@
+﻿
+
+$(document).ready(function () {
+    var counter = 0;
+
+    $("#addrow").on("click", function () {
+        var newRow = $("<tr>");
+        var cols = "";
+
+        cols += '<td></td>';
+        cols += '<td></td>';
+        cols += '<td></td>';
+        cols += '<td></td>';
+        newRow.append(cols);
+        $("table.order-list").append(newRow);
+    });
+
+});
+
+
+
+function calculateRow(row) {
+    var price = +row.find('input[name^="price"]').val();
+
+}
+
+function calculateGrandTotal() {
+    var grandTotal = 0;
+    $("table.order-list").find('input[name^="price"]').each(function () {
+        grandTotal += +$(this).val();
+    });
+    $("#grandtotal").text(grandTotal.toFixed(2));
+}
