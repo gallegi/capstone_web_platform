0fcf42c1fbe839f1df1cfd11cf136dc526b38d61
sonnh <yggdrasil2769@gmail.com>
2019-11-14 19:54:03 +0700

    Update DocumentNotReceivedController.cs

--- vnpost_ocr_system/Controllers/Document/DocumentNotReceivedController.cs
+++ vnpost_ocr_system/Controllers/Document/DocumentNotReceivedController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using vnpost_ocr_system.Models;
 
 namespace vnpost_ocr_system.Controllers.Document
 {
@@ -12,6 +13,12 @@ namespace vnpost_ocr_system.Controllers.Document
         [Route("ho-so/ho-so-cho-nhan")]
         public ActionResult Index()
         {
+            using(VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities())
+            {
+                List<Province> proList = db.Provinces.ToList();
+                ViewBag.proList = proList;
+                List<District> disList = db.Districts.ToList();
+            }
             return View("/Views/Document/DocumentNotReceived.cshtml");
         }
     }
