3fc3cb73c2afb55243378e86c73079373a854ecd
thang1160 <thang1160@users.noreply.github.com>
2019-11-14 19:38:08 +0700

    Màn tạo giấy hẹn cho khách hàng

---
+++ vnpost_ocr_system/Controllers/GetProvince/GetProvincesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vnpost_ocr_system.Models;
+
+namespace vnpost_ocr_system.Controllers.GetProvince
+{
+    public class GetProvincesController : Controller
+    {
+        // GET: GetProvinces
+        [Route("GetDistrict")]
+        [HttpPost]
+        public ActionResult Index(string code)
+        {
+            VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
+            List<District> list = db.Districts.Where(x => x.PostalProvinceCode.Equals(code)).ToList();
+            return Json(new { data = list});
+        }
+    }
+}
