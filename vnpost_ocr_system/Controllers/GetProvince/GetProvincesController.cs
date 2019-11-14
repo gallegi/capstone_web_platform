071781b2164a68eb60a8c6e5a861909c5c9f390e
thang1160 <thang1160@users.noreply.github.com>
2019-11-15 00:03:51 +0700

    thêm lấy danh sách province, district,...

--- vnpost_ocr_system/Controllers/GetProvince/GetProvincesController.cs
+++ vnpost_ocr_system/Controllers/GetProvince/GetProvincesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,17 +15,39 @@ namespace vnpost_ocr_system.Controllers.GetProvince
         public ActionResult GetDistrictByProvinceCode(string code)
         {
             VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
-            List<District> list = db.Districts.Where(x => x.PostalProvinceCode.Equals(code)).ToList();
-            return Json(new { data = list});
+            //3 cái đều chạy được, select để lọc data thừa trả về view cho nhẹ, dùng hay không thì tùy
+            //List<District> list = db.Districts.Where(x => x.PostalProvinceCode.Equals(code)).Select(x => new District
+            //{
+            //    PostalDistrictCode = x.PostalDistrictCode,
+            //    PostalDistrictName = x.PostalDistrictName
+            //}).ToList();
+
+            //List<District> list = (from d in db.Districts
+            //                       where d.PostalProvinceCode.Equals(code)
+            //                       select d).ToList().Select(x => new District
+            //                       {
+            //                           PostalDistrictCode = x.PostalDistrictCode,
+            //                           PostalDistrictName = x.PostalDistrictName
+            //                       }).ToList();
+            List<District> list = db.Database.SqlQuery<District>("select * from District where PostalProvinceCode = @code", new SqlParameter("code", code)).ToList()
+                .Select(x => new District {
+                    PostalDistrictCode = x.PostalDistrictCode,
+                    PostalDistrictName = x.PostalDistrictName
+                }).ToList();
+            return Json(list);
         }
 
-        [Route("GetPostOffice")]
+        [Route("GetProfile")]
         [HttpPost]
-        public ActionResult GetPostOfficeByDistrictCode(string code)
+        public ActionResult GetProfileByPAId(string code)
         {
             VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
-            List<PostOffice> list = db.PostOffices.Where(x => x.DistrictCode.Equals(code)).ToList();
-            return Json(new { data = list });
+            List<Profile> list = db.Profiles.Where(x => x.PublicAdministrationLocationID.Equals(code)).ToList().Select(x => new Profile
+            {
+                ProfileID = x.ProfileID,
+                ProfileName = x.ProfileName
+            }).ToList();
+            return Json(list);
         }
 
         [Route("GetDetails")]
@@ -34,8 +57,12 @@ namespace vnpost_ocr_system.Controllers.GetProvince
             VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
             List<PublicAdministration> list = (from po in db.PostOffices.Where(x => x.DistrictCode.Equals(code))
                                                join pa in db.PublicAdministrations on po.PosCode equals pa.PosCode
-                                               select pa).ToList();
-            return Json(new { data = list });
+                                               select pa).ToList().Select(x => new PublicAdministration
+                                               {
+                                                   PublicAdministrationLocationID = x.PublicAdministrationLocationID,
+                                                   PublicAdministrationName = x.PublicAdministrationName
+                                               }).ToList();
+            return Json(list);
         }
     }
 }
