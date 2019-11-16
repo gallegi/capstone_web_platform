de2631de83aa800885b1ce41ab5d258e21d71b0d
Long Vu <vulong22121999@gmail.com>
2019-11-16 20:07:02 +0700

    back-end 11.1

--- vnpost_ocr_system/Controllers/Document/DocumentReceivedDetailController.cs
+++ vnpost_ocr_system/Controllers/Document/DocumentReceivedDetailController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using vnpost_ocr_system.Models;
 namespace vnpost_ocr_system.Controllers.Document
 {
     public class DocumentReceivedDetailController : Controller
@@ -12,6 +12,12 @@ namespace vnpost_ocr_system.Controllers.Document
         [Route("ho-so/ho-so-da-nhan/chi-tiet")]
         public ActionResult Index()
         {
+            String query = "select * from [Order] where OrderID=1";
+            VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
+            Order order = db.Orders.Find(1);
+            ViewBag.statusID = order.StatusID;
+            ViewBag.orderID = order.OrderID;
+            ViewBag.order = order;
             return View("/Views/Document/DocumentReceivedDetail.cshtml");
         }
     }
