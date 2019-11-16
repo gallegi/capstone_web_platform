c5ecdf46c510b83b9ceb5c1f6734edc253ef675a
tuan211199 <tuan211199@gmail.com>
2019-11-16 10:16:47 +0700

    them backend man11 (non-complete)

--- vnpost_ocr_system/Controllers/Document/DocumentRecivedController.cs
+++ vnpost_ocr_system/Controllers/Document/DocumentRecivedController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using vnpost_ocr_system.Models;
 
 namespace vnpost_ocr_system.Controllers.Document
 {
@@ -12,7 +13,36 @@ namespace vnpost_ocr_system.Controllers.Document
         [Route("ho-so/ho-so-da-nhan")]
         public ActionResult Index()
         {
-            return View("/Views/Document/DocumentRecived.cshtml");
+                using(VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities())
+            {
+                List<Province> proList = db.Provinces.ToList();
+                ViewBag.proList = proList;
+
+            }
+                return View("/Views/Document/DocumentRecived.cshtml");
+        }
+
+
+        public class recieve : Order
+        {
+            public string PostalProvinceName { get; set; }
+            public string ProfileName { get; set; }
+            public string PublicAdministrationName { get; set; }
+            public string Phone { get; set; }
+        }
+
+        [Route("da-tiep-nhan")]
+        [HttpPost]
+        public ActionResult Search(string province, string district, string profile, string )
+        {
+            VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
+            List<recieve> searchList = null;
+            int totalrows = 0;
+            int totalrowsafterfiltering = 0;
+            string query = "";
+
+            query += "Select * From "
+
         }
     }
 }
