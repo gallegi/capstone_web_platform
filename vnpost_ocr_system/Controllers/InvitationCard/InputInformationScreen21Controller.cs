da1d64ecb3203c07a404b60e977f614b6b697d40
thang1160 <thang1160@users.noreply.github.com>
2019-11-23 17:12:17 +0700

    cập nhật tạo profile trên stepper, sửa datepicker hiển thị sau modal

--- vnpost_ocr_system/Controllers/InvitationCard/InputInformationScreen21Controller.cs
+++ vnpost_ocr_system/Controllers/InvitationCard/InputInformationScreen21Controller.cs
@@ -44,12 +44,14 @@ namespace vnpost_ocr_system.Controllers.InvitationCard
                 string FullName = Request["FullName"];
                 string Phone = Request["Phone"];
                 string PostalDistrictCode = Request["PostalDistrictCode"];
+                string PostalDistrictName = Request["PostalDistrictName"];
                 string Street = Request["Street"];
                 string PersonalPaperTypeID = Request["PersonalPaperTypeID"];
                 string PersonalPaperNumber = Request["PersonalPaperNumber"];
                 string PersonalPaperIssuedDateString = Request["PersonalPaperIssuedDateString"];
                 string PersonalPaperIssuedPlace = Request["PersonalPaperIssuedPlace"];
                 string ContactInfoID = Request["ContactInfoID"];
+                string PostalProvinceName = Request["PostalProvinceName"];
                 string Type = Request["Type"];  //1 có đủ thông tin, 2 thiếu giấy tờ tùy thân
                 using (VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities())
                 {
@@ -63,14 +65,51 @@ namespace vnpost_ocr_system.Controllers.InvitationCard
                     {
                         c.PersonalPaperTypeID = int.Parse(PersonalPaperTypeID);
                         c.PersonalPaperNumber = PersonalPaperNumber;
-                        c.PersonalPaperIssuedDate = DateTime.ParseExact(PersonalPaperIssuedDateString, "dd/MM/yyyy", null);
+                        if (PersonalPaperIssuedDateString == "")
+                            c.PersonalPaperIssuedDate = null;
+                        else
+                            c.PersonalPaperIssuedDate = DateTime.ParseExact(PersonalPaperIssuedDateString, "dd/MM/yyyy", null);
                         c.PersonalPaperIssuedPlace = PersonalPaperIssuedPlace;
                     }
                     if (ContactInfoID == "")
                     {
                         c.CustomerID = long.Parse(Session["userID"].ToString());
                         db.ContactInfoes.Add(c);
-                        return Json(new { success = true, add = true, message = "Thêm mới thành công" });
+                        db.SaveChanges();
+                        string html = @"<div id='contact1' class='info-list-div col'>
+                                        <div class='col s8 m8 l8'>
+                                            <p id='FullName' class='content-text highlight col s12'>" + c.FullName + @"</p>
+                                            <p class='content-text col s12'><span id='Street'>" + c.Street + @"</span>, <span data-district='" + c.PostalDistrictCode + @"' id='Address'>" + PostalDistrictName + @", " + PostalProvinceName + @"</span></p>
+                                            <p class='content-text col s12'>Số điện thoại: <span id='Phone'>" + c.Phone + @"</span></p>";
+                        html += Type == "2"? @"<p class='col s12 big-heading m-b-15'>
+                                Bạn muốn sử dụng thông tin khác?
+                                <a class='modal-trigger light-blue-text hover-underline' href='#myform1' id='add' data-type='2'>
+                                    Nhập thông tin mới
+                                </a>
+                            </p>" : @"
+                                            <p class='content-text col s12'>Loại giấy tờ tùy thân: <span data-papertype='" + c.PersonalPaperTypeID + @"' id='PersonalPaperTypeName'>" + c.Street + @"</span></p>
+                                            <p class='content-text col s12'>Số giấy tờ tùy thân: <span id='PersonalPaperNumber'>" + c.PersonalPaperNumber + @"</span></p>
+                                            <p class='content-text col s12'>Ngày cấp: <span id='PersonalPaperIssuedDate'>" + PersonalPaperIssuedDateString + @"</span></p>
+                                            <p class='content-text col s12'>Nơi cấp: <span id='PersonalPaperIssuedPlace'>" + c.PersonalPaperIssuedPlace + @"</span></p>
+                                        </div>
+                                        <div class='col l4 s4 m4 is-check-step-2' id='isCheckStep2-" + c.ContactInfoID + @"'>
+                                        </div>
+                                        <div class='col s12 m-t-10'>
+                                            <div class='col l4 s12 p-t-10'>
+                                                <a class='btn waves-effect waves-light bt-color-common' data-profile1='" + c.ContactInfoID + @"' id='profile1'>Sử dụng thông tin này</a>
+                                            </div>
+                                            <div class='col l2 s12 p-t-10'>
+                                                <a class='modal-trigger btn waves-effect waves-light bt-color-common' data-profile1='" + c.ContactInfoID + @"' data-type='1' id='edit' href='#myform1'>Chỉnh sửa</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <p class='col s12 big-heading m-b-15'>
+                                Bạn muốn sử dụng thông tin khác?
+                                <a class='modal-trigger light-blue-text hover-underline' href='#myform1' id='add' data-type='1'>
+                                    Nhập thông tin mới
+                                </a>
+                            </p>";
+                        return Json(new { success = true, add = true, message = "Thêm mới thành công", html = html });
                     }
                     db.SaveChanges();
                     return Json(new { success = true, add = false, message = "Chỉnh sửa thành công" });
