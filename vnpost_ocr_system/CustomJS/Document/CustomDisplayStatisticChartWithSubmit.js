624d57c488702bd2865e8804b83f58baffed8840
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2019-12-04 10:36:59 +0700

    Cập nhật js dashboard

--- vnpost_ocr_system/CustomJS/Document/CustomDisplayStatisticChartWithSubmit.js
+++ vnpost_ocr_system/CustomJS/Document/CustomDisplayStatisticChartWithSubmit.js
@@ -30,66 +30,7 @@
         $('#profile_chy').val(text);
         $('#profile_chm').val(text);
     }
-    function change() {
-        $('#pre-load').show()
-        var text = $("#item1 option:selected").html();
-        //if (text == "Tất cả") {
-        //    $('.tk1').hide();
-        //    $('.tk2').hide();
-        //    $('.tk3').hide();
-        //    $('.tk4').hide();
-        //} else {
-        //    city(text);
-        //    $('.tk1').show();
-        //}
-        $.ajax({
-            type: "POST",
-            url: "/GetDistrict",
-            cache: false,
-            dataType: 'json',
-            data: {
-                code: $(this).val()
-            },
-            success: function (response) {
-                $('#item2').empty();
-                var option = "<option value='Tất cả' selected>Tất cả</option>";
-                for (var i = 0; i < response.length; i++) {
-                    option += "<option value='" + response[i].PostalDistrictCode + "'>" + response[i].PostalDistrictName + "</option>";
-                }
-                $('#item2').append(option);
-                $('#item2').formSelect();
-
-                $('#item3').empty();
-                var option = "<option value='Tất cả' selected>Tất cả</option>";
-                $('#item3').append(option);
-                $('#item3').formSelect();
-
-                $('#item4').empty();
-                var option = "<option value='Tất cả' selected>Tất cả</option>";
-                $('#item4').append(option);
-                $('#item4').formSelect();
-
-                //$('.tk2').hide();
-                //$('.tk3').hide();
-                //$('.tk4').hide();
-                $('#pre-load').hide();
-
-            },
-            error: function () {
-                $('#pre-load').hide()
-            }
-        });
-        $("div.it2 select").val("Tất cả");
-        $("div.it3 select").val("Tất cả");
-        $("div.it4 select").val("Tất cả");
-        getText();
-        $('#formall').submit();
-        change1();
-        change2();
-        $('#form3').submit();
-        $("#cmonth").hide();
-        $("#cyear").show();
-    }
+    
 
     $('#item1').change(function () {
         change();
@@ -113,7 +54,7 @@
             cache: false,
             dataType: 'json',
             data: {
-                code: $(this).val()
+                code: $("#item2").val()
             },
             success: function (response) {
                 $('#item3').empty();
@@ -165,7 +106,7 @@
             cache: false,
             dataType: 'json',
             data: {
-                code: $(this).val()
+                code: $("#item3").val()
             },
             success: function (response) {
                 $('#item4').empty();
