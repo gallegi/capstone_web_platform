d9e374baf5dd9d7310fd6711978a1ed0e4417864
duongpl <51127228+duongpl@users.noreply.github.com>
2019-12-28 23:38:57 +0700

    fix account

--- vnpost_ocr_system/Controllers/Login/ManagementUserController.cs
+++ vnpost_ocr_system/Controllers/Login/ManagementUserController.cs
@@ -246,41 +246,49 @@ namespace vnpost_ocr_system.Controllers.Login
         }
         public ActionResult GenUsername(int id, string province)
         {
-            string username = "sample";
-            var provincedb = db.Provinces.Where(x => x.PostalProvinceCode.Equals(province)).FirstOrDefault();
-            if (id == 2)
+            try
             {
-                username = "admin00"; var tct = db.Admins.Where(x => x.Role == 2).ToList();
-                if (tct.Count == 1) { username += "_1"; }
-                if (tct.Count > 1)
+                string username = "sample";
+                var provincedb = db.Provinces.Where(x => x.PostalProvinceCode.Equals(province)).FirstOrDefault();
+                if (id == 2)
                 {
-                    string tctt = tct.Last().AdminUsername;
-                    username += "_" + (Convert.ToInt32(tctt.Substring(8, tctt.Length - 8)) + 1);
+                    username = "admin00"; var tct = db.Admins.Where(x => x.Role == 2).ToList();
+                    if (tct.Count == 1) { username += "_1"; }
+                    if (tct.Count > 1)
+                    {
+                        string tctt = tct.Last().AdminUsername;
+                        username += "_" + (Convert.ToInt32(tctt.Substring(8, tctt.Length - 8)) + 1);
+                    }
                 }
+                else if (id == 3)
+                {
+                    username = "admin_" + provincedb.ProvinceShortName; var adt = db.Admins.Where(x => x.Role == 3 && x.PostalProvinceCode.Equals(province)).ToList();
+                    if (adt.Count == 0) { username += "_1"; }
+                    if (adt.Count >= 1)
+                    {
+                        int ind = username.Length;
+                        string adttt = adt.Last().AdminUsername;
+                        username += "_" + (Convert.ToInt32(adttt.Substring(ind + 1, adttt.Length - ind - 1)) + 1);
+                    }
+                }
+                else if (id == 4)
+                {
+                    username = "giaodichvien_" + provincedb.ProvinceShortName; var gdv = db.Admins.Where(x => x.Role == 4 && x.PostalProvinceCode.Equals(province)).ToList();
+                    if (gdv.Count == 0) { username += "_1"; }
+                    if (gdv.Count >= 1)
+                    {
+                        int ind = username.Length;
+                        string gdvv = gdv.Last().AdminUsername;
+                        username += "_" + (Convert.ToInt32(gdvv.Substring(ind + 1, gdvv.Length - ind - 1)) + 1);
+                    }
+                }
+                return Json(username, JsonRequestBehavior.AllowGet);
             }
-            else if (id == 3)
+            catch(Exception ex)
             {
-                username = "admin_" + provincedb.ProvinceShortName; var adt = db.Admins.Where(x => x.Role == 3 && x.PostalProvinceCode.Equals(province)).ToList();
-                if (adt.Count == 0) { username += "_1"; }
-                if (adt.Count >= 1)
-                {
-                    int ind = username.Length;
-                    string adttt = adt.Last().AdminUsername;
-                    username += "_" + (Convert.ToInt32(adttt.Substring(ind + 1, adttt.Length - ind - 1)) + 1);
-                }
+                return Json("", JsonRequestBehavior.AllowGet);
             }
-            else if (id == 4)
-            {
-                username = "giaodichvien_" + provincedb.ProvinceShortName; var gdv = db.Admins.Where(x => x.Role == 4 && x.PostalProvinceCode.Equals(province)).ToList();
-                if (gdv.Count == 0) { username += "_1"; }
-                if (gdv.Count >= 1)
-                {
-                    int ind = username.Length;
-                    string gdvv = gdv.Last().AdminUsername;
-                    username += "_" + (Convert.ToInt32(gdvv.Substring(ind + 1, gdvv.Length - ind - 1)) + 1);
-                }
-            }
-            return Json(username, JsonRequestBehavior.AllowGet);
+
         }
     }
     public class Admindb
