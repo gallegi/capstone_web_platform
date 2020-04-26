3009cef3882ceb975de78c4aab2e45600b740d6e
Vinh Lương <stuncb97@gmail.com>
2020-04-27 06:22:11 +0700

    Tách models khỏi controller + sửa icon button ở màn xem các order đã tiếp nhận (admin) từ edit -> view

--- vnpost_ocr_system/Controllers/Mobile/MobileAppController.cs
+++ vnpost_ocr_system/Controllers/Mobile/MobileAppController.cs
@@ -223,24 +223,7 @@ namespace vnpost_ocr_system.Controllers.Mobile
             }
             return s;
         }
-        class NotiMessage
-        {
-            public long orderID { get; set; }
-            public string Title { get; set; }
-            public string ContentText { get; set; }
-            private string _sendate;
-            public string SentDate
-            {
-                //Apr 15 2020  4:25AM
-                get
-                {
-                    if (_sendate == "") return "";
-                    else return DateTime.Parse(_sendate).ToString("dd/MM/yyyy HH:mm:ss");
-                }
-
-                set { _sendate = value; }
-            }
-        }
+       
         public static void SendFCMMessage(long orderID, long statusID)
         {
             List<string> deviceTokens = new List<string>();
