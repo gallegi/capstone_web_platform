0cc08619ebe2b1a7f9c89a86be0cf72419996105
MinhNV <nguyenvuminh98@gmail.com>
2020-05-05 23:04:41 +0700

    Fix bug form list page #200

--- vnpost_ocr_system/Controllers/Form/ListFormController.cs
+++ vnpost_ocr_system/Controllers/Form/ListFormController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using vnpost_ocr_system.Models;
+using vnpost_ocr_system.SupportClass;
 
 namespace vnpost_ocr_system.Form
 {
@@ -9,6 +10,7 @@ namespace vnpost_ocr_system.Form
 
         private VNPOST_AppointmentEntities db = new VNPOST_AppointmentEntities();
         // GET: ListForm
+        [Auther(Roles = "1")]
         [Route("bieu-mau/list-bieu-mau")]
         public ActionResult Index()
         {
