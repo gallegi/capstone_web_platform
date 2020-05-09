038fe0baf6d4444b702b1a0e1ce342b6692353b6
MinhNV <nguyenvuminh98@gmail.com>
2020-05-09 22:38:38 +0700

    Add remove accent function

--- vnpost_ocr_system/dist/js/custom.js
+++ vnpost_ocr_system/dist/js/custom.js
@@ -1,4 +1,4 @@
-// jQuery
+﻿// jQuery
 
 $(function() {
 
@@ -233,6 +233,31 @@ $(function() {
         return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
     }
 
+    function removeAccents(str) {
+        var AccentsMap = [
+            "aàảãáạăằẳẵắặâầẩẫấậ",
+            "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+            "dđ", "DĐ",
+            "eèẻẽéẹêềểễếệ",
+            "EÈẺẼÉẸÊỀỂỄẾỆ",
+            "iìỉĩíị",
+            "IÌỈĨÍỊ",
+            "oòỏõóọôồổỗốộơờởỡớợ",
+            "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+            "uùủũúụưừửữứự",
+            "UÙỦŨÚỤƯỪỬỮỨỰ",
+            "yỳỷỹýỵ",
+            "YỲỶỸÝỴ"
+        ];
+        for (var i = 0; i < AccentsMap.length; i++) {
+            var re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
+            var char = AccentsMap[i][0];
+            str = str.replace(re, char);
+        }
+        return str;
+    }
+
+
     $('.dynamic-color .col').each(function() {
         $(this).children().each(function() {
             var color = $(this).css('background-color'),
@@ -290,3 +315,4 @@ $(function() {
         $('.show-left-part').toggleClass('ti-menu');
     });
 });
+
