859c78d806bad981fcc11129aa00b02f56f97c78
MinhNV <nguyenvuminh98@gmail.com>
2020-05-09 23:37:07 +0700

    Fix bug #207: Validate Vietnamese name

--- vnpost_ocr_system/dist/js/custom.js
+++ vnpost_ocr_system/dist/js/custom.js
@@ -233,30 +233,6 @@ $(function() {
         return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
     }
 
-    function removeAccents(str) {
-        var AccentsMap = [
-            "aàảãáạăằẳẵắặâầẩẫấậ",
-            "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
-            "dđ", "DĐ",
-            "eèẻẽéẹêềểễếệ",
-            "EÈẺẼÉẸÊỀỂỄẾỆ",
-            "iìỉĩíị",
-            "IÌỈĨÍỊ",
-            "oòỏõóọôồổỗốộơờởỡớợ",
-            "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
-            "uùủũúụưừửữứự",
-            "UÙỦŨÚỤƯỪỬỮỨỰ",
-            "yỳỷỹýỵ",
-            "YỲỶỸÝỴ"
-        ];
-        for (var i = 0; i < AccentsMap.length; i++) {
-            var re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
-            var char = AccentsMap[i][0];
-            str = str.replace(re, char);
-        }
-        return str;
-    }
-
 
     $('.dynamic-color .col').each(function() {
         $(this).children().each(function() {
